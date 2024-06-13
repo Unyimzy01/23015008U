4d0771303131d984a9114f7a8bfad5a353d02335
Unyimzy01 <unyimematthew2020@gmail.com>
2024-06-13 12:11:08 +0100

    emailvalidator.py

--- emailvalidator.py
+++ emailvalidator.py
@@ -1,10 +1,10 @@
 import re
-mail = input("Enter your email address: ")
+address = input("Enter your email address: ")
     # Regular expression for validating email addresses
-mail_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
 # Check if the email matches the pattern
-if re.match(mail_regex, mail):
+if re.match(regex, address):
      print("valid email")
 else:
-    print('not matched to regular email')
+    print('not matched ')
